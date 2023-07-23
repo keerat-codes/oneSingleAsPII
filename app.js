6f2d640b9a42c03710c2814aca2ca7f10ed23f59
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2023-07-24 01:48:01 +0530

    Updated app.js using AutoPilot

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cube  = require('./controllers/'cube);
 const translate  = require('./controllers/'translate);
 const hiBarbie  = require('./controllers/'hiBarbie);
 const coinToss  = require('./controllers/coinToss');
+const newYear  = require('./controllers/newYear');
+app.use('/newYear', newYear);
 app.use('/toss', coinToss);
 app.use('/Barbie', hiBarbie);
 app.use('/translate', translate);
